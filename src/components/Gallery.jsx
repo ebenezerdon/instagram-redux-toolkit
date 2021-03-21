d4b095b89bee72d000b299bc98da6629df9f66ea
Ebenezer Don <ebenezerdonu@gmail.com>
2021-03-21 16:02:39 +0100

    Initial commit

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { posts } from '../data'
+
+const Gallery = () => {
+  const galleryItems = posts.map(post => (
+    <div className="item" key={post.id} tabIndex={8}>
+      <img src={post.imageUrl} alt="" />
+    </div>
+  ))
+
+  return (
+    <>
+      <input type="file" name="photo" id="addPhotoInput" />
+      <label form="addPhotoInput">
+        <i className="add-photo fas fa-plus-square" />
+      </label>
+      <div className="gallery-nav" />
+      <section className="gallery">
+        {galleryItems}
+      </section>
+    </>
+  )
+}
+
+export default Gallery
