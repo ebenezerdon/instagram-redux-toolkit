f8afc3627f41a926ed255217ae537186654e2526
Ebenezer Don <ebenezerdonu@gmail.com>
2021-03-21 19:44:32 +0100

    fix indentation

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -7,8 +7,8 @@ const Bio = () => {
           <img src="https://images.pexels.com/photos/1727280/pexels-photo-1727280.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="profile" />
         </div>
         <div className="profile-info">
-            <p className="name">Jane Butters</p>
-            <button>follow</button>
+          <p className="name">Jane Butters</p>
+          <button>follow</button>
         </div>
       </section>
     )
