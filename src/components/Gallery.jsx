60587aa4bc096ddc5b970920cf1a1af6103995dc
Ebenezer Don <ebenezerdonu@gmail.com>
2021-03-21 19:44:50 +0100

    use redux state in Gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
-import { posts } from '../data'
+import { useDispatch, useSelector } from 'react-redux'
+import getPhotoUrl from 'get-photo-url'
+import { galleryActions } from "../data";
 
 const Gallery = () => {
-  const galleryItems = posts.map(post => (
+  const galleryState = useSelector(state => state.gallery)
+  const dispatch = useDispatch()
+
+  const addPhoto = async () => {
+    const imageUrl = await getPhotoUrl('#addPhotoInput')
+    const post = {
+      id: '',
+      imageUrl,
+      text: ''
+    }
+
+    dispatch(galleryActions.addPost(post))
+  }
+
+  const galleryItems = galleryState.posts?.map(post => (
     <div className="item" key={post.id} tabIndex={8}>
       <img src={post.imageUrl} alt="" />
     </div>
@@ -11,8 +27,8 @@ const Gallery = () => {
   return (
     <>
       <input type="file" name="photo" id="addPhotoInput" />
-      <label form="addPhotoInput">
-        <i className="add-photo fas fa-plus-square" />
+      <label htmlFor="addPhotoInput">
+        <i className="add-photo fas fa-plus-square" onClick={addPhoto} />
       </label>
       <div className="gallery-nav" />
       <section className="gallery">
